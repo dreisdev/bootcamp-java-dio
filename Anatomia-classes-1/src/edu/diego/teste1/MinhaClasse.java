895b84d1f0be4f6be78a54196135783df6dc0c83
dreisdev <dreis.dev@outlook.com>
2023-03-28 21:17:28 -0300

    creating project for example bootcamp java.

---
+++ Anatomia-classes-1/src/edu/diego/teste1/MinhaClasse.java
@@ -0,0 +1,17 @@
+package edu.diego.teste1;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        int a = 32;
+
+        int b = 56;
+
+        int resultado = a + b;
+
+        System.out.println(resultado);
+
+    }
+
+}
