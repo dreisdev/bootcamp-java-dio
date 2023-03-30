d466c9c202cabdfb41a738de6932750f8f73b05b
dreisdev <dreis.dev@outlook.com>
2023-03-29 22:13:26 -0300

    continuation of class. Java Operators

---
+++ Tipos-Variaveis/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        int salario1 = 1000;
+
+        int salario2 = 900;
+
+        String resultadoSalarios = (salario1 > salario2) ? "Verdade" : "Falso";
+
+        System.out.println(resultadoSalarios);
+
+    }
+
+}
--- Tipos-Variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String nomeFuncionario = "Diego Reis";
+
+        double salario = 8330.50;
+
+        double atualizacaoSalario = salario * 1.35;
+
+        atualizacaoSalario = salario * 1.40;
+
+        System.out.println(nomeFuncionario);
+        System.out.println(atualizacaoSalario);
+
     }
 }
